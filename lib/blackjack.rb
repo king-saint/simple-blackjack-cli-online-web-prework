2749d4d44c73766fd99b119bc01996e4812c74e8
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-22 17:08:15 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -20,8 +20,8 @@ def get_user_input
   answer = gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}.  Thanks for playing!"
 end
 
 def initial_round
