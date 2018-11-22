788e60f0a17eb1e883bb0c2800aa636088139e67
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-22 17:18:24 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -4,7 +4,7 @@ end
 
 def deal_card
   num = rand(12)
-  num
+  return num
 end
 
 def display_card_total(sum)
@@ -14,7 +14,7 @@ end
 
 
 def prompt_user
-  puts "Type 'h' to hit or 's' to stay."
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
@@ -22,7 +22,7 @@ def get_user_input
 end
 
 def end_game(card_total)
-  puts "Sorry, you hit #{card_total}.  Thanks for playing!"
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
